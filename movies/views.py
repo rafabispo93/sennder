3b7e214cfee8b9bf1bb89ae3a2e3975db8a25ddd
Rafael <rafaelbispo993@gmail.com>
2020-07-16 19:03:52 -0300

    Add people to the table

--- movies/views.py
+++ movies/views.py
@@ -8,12 +8,29 @@ def movies_list(request):
     try:
         is_cached = ('films' in request.session)
         if not is_cached:
-            response = requests.get(API_URL + 'films')
-            request.session['films'] = response.json()
+            request.session['films'] = _get_people_in_films(requests.get(API_URL + 'films').json())
         films = request.session['films']
         return render(request,
                       'movies/movies_list.html',
                       {'films': films})
     except Exception as error:
         print(error)
-        return "Failed to retrieve data, please try again."
+        return 'Failed to retrieve data, please try again.'
+
+
+def _get_people_in_films(films):
+    people = requests.get(API_URL + 'people').json()
+    people_film = []
+    for person in people:
+        for f in person['films']:
+            people_film.append({
+                'id': f.split('films/')[1],
+                'name': person['name']
+            })
+    for film in films:
+        film['people'] = []
+        for p in people_film:
+            if p['id'] == film['id']:
+                if p['name'] not in film['people']:
+                    film['people'].append(p['name'])
+    return films
