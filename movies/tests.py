53273fb3d3a60595bc20d4d87f0e820a4c3c528f
Rafael <rafaelbispo993@gmail.com>
2020-07-18 20:28:51 -0300

    Add first version of unit tests

--- movies/tests.py
+++ movies/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.urls import reverse
+from movies import views
+from django.contrib.sessions.middleware import SessionMiddleware
 
-# Create your tests here.
+
+class MoviesTest(TestCase):
+
+    def test_movies_list_view(self):
+
+        url = reverse(views.movies_list)
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertIsNotNone(resp.content)
+
+    def test_get_films(self):
+
+        # request = RequestFactory().get('http://localhost:8000/movies/')
+        # middleware = SessionMiddleware()
+        # middleware.process_request(request)
+        # request.session.save()
+        # response = views.movies_list(request)
+        films = views._get_films_ordered_by_most_recent()
+        self.assertIsNot(len(films), 0)
+        for film in films:
+            self.assertTrue('people' in film.keys())
--- movies/views.py
+++ movies/views.py
@@ -8,31 +8,52 @@ def movies_list(request):
     try:
         is_cached = ('films' in request.session)
         if not is_cached:
-            request.session['films'] = _get_people_in_films(
-                requests.get(API_URL + 'films').json())
+            request.session['films'] = _get_films_ordered_by_most_recent()
         films = request.session['films']
         return render(request,
                       'movies/movies_list.html',
                       {'films': films})
     except Exception as error:
         print(error)
-        return 'Failed to retrieve data, please try again.'
+        return error
+
+
+def _get_films_ordered_by_most_recent():
+    # Get the films and order by the most recent
+    try:
+        filter_data = {
+            'limit': 250,
+            'fields': 'id,title,description,director,producer,release_date,rt_score'
+        }
+        return sorted(_get_people_in_films(
+            requests.get(API_URL + 'films', filter_data).json()), key=lambda k: k['release_date'], reverse=True)
+    except Exception as error:
+        print(error)
+        return error
 
 
 def _get_people_in_films(films):
-    people = requests.get(API_URL + 'people').json()
-    people_film = []
-    for person in people:
-        for f in person['films']:
-            people_film.append({
-                'id': f.split('films/')[1],
-                'name': person['name']
-            })
-    for film in films:
-        film['people'] = []
-        for p in people_film:
-            if p['id'] == film['id']:
-                if p['name'] not in film['people']:
-                    film['people'].append(p['name'])
-        film['people'] = ', '.join(film['people'])
-    return films
+    try:
+        filter_data = {
+            'limit': 250,
+            'fields': 'id,name,films'
+        }
+        people = requests.get(API_URL + 'people', filter_data).json()
+        people_film = []
+        for person in people:
+            for f in person['films']:
+                people_film.append({
+                    'id': f.split('films/')[1],
+                    'name': person['name']
+                })
+        for film in films:
+            film['people'] = []
+            for p in people_film:
+                if p['id'] == film['id']:
+                    if p['name'] not in film['people']:
+                        film['people'].append(p['name'])
+            film['people'] = ', '.join(film['people'])
+        return films
+    except Exception as error:
+        print(error)
+        return error
